2ed80e6f436602f758161b0ca9fa2bf99a422393
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-04 17:46:01 +0200

    h

--- Acron.php
+++ Acron.php
@@ -1,10 +1,5 @@
 <?php
-@ob_start();
-@session_start();
-include 'inc/config.php';
-$lg='ar';
-include"inc/lang.php";
-include "inc/function.php" ;
+include 'inc.php';
 $St=getSet();
 $a= array($St->url."/cron.php?quran=true",$St->url."/cron.php?post=true");
 for($i=0;$i<count($a);$i++){
