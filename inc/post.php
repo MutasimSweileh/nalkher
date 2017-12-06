65c170f2e8343df02e358e37cc46c7a5a05775af
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-06 16:02:12 +0200

    goo

--- inc/post.php
+++ inc/post.php
@@ -195,7 +195,7 @@ if($p['num_dw']){
                                     <div class="col  m2 s4 right center">
                 					    <a href=""><img width="43" height="43"  src="<?=FbImg(Sion('id'),'normal')?>" class="circle img-responsive"></a>
                                     </div>
-                                    <div class="col s8 m10 ">
+                                    <div class="input-field  col s8 m10  textfilde">
 										<textarea class="form-control CommentBox materialize-textarea"  name="comment" id="<?=$id?>"  ></textarea>
                                         <label for="<?=$id?>" class="">اكتب تعليقك</label>
                                         <input type="hidden" name="Cuid" value="<?=Sion('id')?>" />
