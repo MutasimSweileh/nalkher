03193a758378c777f6f45a5fbf575257416b7b13
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-07 15:04:33 +0200

    jj

--- test.php
+++ test.php
@@ -1,19 +1,18 @@
 <?php
 include "inc.php";
 
- $post = getUser("posts"," order by id desc ");
+ $post = getUser("posts"," order by id desc limit 5 ");
  $text = "";
 for($i = 0;$i<count($post);$i++){
     $p = $post[$i];
     if($text == $p["text"]){
     Remove("post","where id".$p["id"]);
    }
-
-
   $text = $p["text"];
 
 }
-//echo "string";
+
+echo $text;
 
 
   ?>
