ff31a8263d2e051f11e92063b551736a10044319
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-12 20:27:43 +0200

    s

--- inc/function.php
+++ inc/function.php
@@ -1653,6 +1653,7 @@ if($j["data"]){
   //status
   //http://www.3lmnyonline
  $str =  strpos($j["data"][0]['message'],'3lmny');
+ 
 if($j["data"][0]["type"] == "photo" && !$str){
   $link =  Uimgur($j["data"][0]['full_picture']);
 if($link[0]){ $link = $link[1];  }else {
