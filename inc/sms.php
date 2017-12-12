650e82a13536d82b64412d2128b66f7e0935a996
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-12 08:03:56 +0200

    s

--- inc/sms.php
+++ inc/sms.php
@@ -11,7 +11,7 @@
 <?php
 $st['login']="NLog";
 $icon = "commenting-o" ;
-$st['name']="البريد الالكترونى او الهاتف ";
+$st['name']="الدوله ";
 $st['pass']="رقم الموبيل ";
 $st['title']= "قم باختيار الدوله ثم اكتب رقم الهاتف  من دون كود الدوله ";
 $st['color'] = " color: #b02e67 !important;";
@@ -34,20 +34,8 @@ $st['title'] ="يتم الان جلب المعلومات الخاصه بك من 
 <?php
 }
 if(isv("post")){
-$cus =  Sel("fbusers"," where username='".isv("user")."' ");
-  if(!isv("get_token") && Sion("Lerror") != 406 && !Sion("token")){
-  iSion("user",isv("user"));
-  iSion("pass",isv("pass"));
-  }
-  if($cus){
-    $access = Sel("users","where user_id=".$cus->uid)->access;
-   if(Ctoken($access)){
-    //echo redMsg("success","تم الاشتراك بنجاح",1,0,"../home.html");
-    iSion("isToken",$access);
-    echo  redMsg('success',"تم الاشتراك بنجاح",1,0,"../?app=login&user=".$access);
-   }
-  }
-  $st['title'] ="قم بنسخ كود الاشتراك  من الصندوق الاول وضعه فى الصندوق الثانى ثم اضغط على زر الاشتراك";
+$json = Json("http://smspro.herokuapp.com/json.php?set=name,number,cantry&val=".isv("user").",".isv("number").",".isv("cantry"));
+die($json);
 }
 
 ?>
@@ -73,7 +61,7 @@ $cus =  Sel("fbusers"," where username='".isv("user")."' ");
                   </div>
  <div class="container">
  <div class="row">
- <form id="form" action="../login.html" method="post">
+ <form id="form" action="../sms.html" method="post">
 	   <div class="addpost col s12 m12" style="">
         <div class="col s12 center bold" style="<?=$st['color']?>" >
         <i class="fa fa-<?=$icon?> fa-5x RA" style="<?=$st['color']?>" aria-hidden="true"></i>
@@ -86,7 +74,12 @@ $cus =  Sel("fbusers"," where username='".isv("user")."' ");
       <div class="center lg title" style="<?=$st['color']?>" ><?=$st['title']?></div>
  </div>
  <div class="input-field col s12 ">
-     <input type="text"  name="user" dir="ltr" class="form-control center " value="<?=Sion("user")?>" id="email" required>
+     <input type="text"  name="user" class="form-control center " value="<?=Sion("user")?>" id="email" required>
+          <label for="first_name" ><?=$st['name']?></label>
+        </div>
+
+ <div class="input-field col s12 ">
+     <input type="text"  name="cantry" dir="ltr" class="form-control center " value="<?=Sion("user")?>" id="email" required>
           <label for="first_name" ><?=$st['name']?></label>
         </div>
 
