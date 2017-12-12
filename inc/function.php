e1a441e6890eada449a195564edaa63e9e004efe
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-12 18:58:48 +0200

    s

--- inc/function.php
+++ inc/function.php
@@ -1763,7 +1763,7 @@ if(getSet()->url != $PUr){
 }
 $us = getUser("posts","where type='0'");
 for ($i=0; $i < count($us); $i++) {
- $str =  strpos($us[$i]['text'],'online');
+ $str =  strpos($us[$i]['text'],'3lmny');
  echo $str."</br>";
 if($str){
 Remove("posts","where id=".$us[$i]['id']);
