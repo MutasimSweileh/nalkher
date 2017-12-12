aec21b009ed8bbb2510849a4bb82579452277cd0
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-13 00:52:03 +0200

    س

--- inc.php
+++ inc.php
@@ -2,8 +2,9 @@
 @ob_start();
 @session_start();
 $lg='ar';
+ini_set('display_errors',1);
 include "inc/config.php";
-include"inc/lang.php";
+include "inc/lang.php";
 include "inc/function.php";
 include_once 'src/facebook.php';
 require_once('src/oauth/twitteroauth.php');
@@ -11,7 +12,7 @@ require_once 'src/API/src/Google/Client.php';
 require_once 'src/API/src/Google/Service/YouTube.php';
 include 'inc/gapi.php';
 $St=getSet();
-date_default_timezone_set("Africa/Cairo"); 
+date_default_timezone_set("Africa/Cairo");
 define("userid","Nuser");
 if(Sion("id"))
 define("userid",Sion("id"));
