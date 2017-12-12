03ebc3836773921ccf6e2e48ace3d020337ee35c
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-12 21:21:25 +0200

    s

--- test.php
+++ test.php
@@ -5,7 +5,7 @@ $nofooter = true;
 
 $us = getUser("posts"," ");
 for ($i=0; $i < count($us); $i++) {
- $str =  strpos($us[$i]['text'],'التفسير');
+ $str =  strpos($us[$i]['text'],'خدمة');
 // echo $us[$i]['text']."</br>";
 if($str){
 $sq =  Remove("posts","where id=".$us[$i]['id']);
