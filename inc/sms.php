82896e330c4f2d6c008e3bb4d389f0af0e8e9e92
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-12 08:40:13 +0200

    س

--- inc/sms.php
+++ inc/sms.php
@@ -76,14 +76,16 @@ die($json);
 
         <div class="input-field col s12">
           <i class="material-icons prefix">location_on</i>
-          <div>
-           <label style="padding-bottom: 5px;">اختر الدولة</label>
+
+
+
            <select  dir="ltr" name="cantryy" class="browser-default">
+               <option value="">اختر الدوله </option>
              <?php  $ar = countre(); for($i=0;$i< count($ar);$i++){ ?>
              <option value="<?=$ar[$i]?>"><?=$ar[$i]?></option>
            <?php } ?>
-         </div>
-         
+
+
          </div>
 
         <div class="input-field col s12 ">
