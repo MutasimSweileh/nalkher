c405d8bd9711e2de04e5c0a39af25331d3a4c8a2
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-13 04:45:20 +0200

    s

--- inc/ajax.php
+++ inc/ajax.php
@@ -2842,7 +2842,7 @@ if(isset($_POST) && isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SER
 }
 
 }else if($_GET['step'] == 'Sms'){
-    if((isset($_POST['number']))&&(isset($_POST['user'])&&$_POST['cantryy']!="")){
+    if(isv("number") && isv("cantryy") && isv("user") ){
       if(strlen(isv("number")) == 11 && isv("cantryy") == "Egypt" || isv("cantryy") != "Egypt" ){
         $number = isv("number");
           if(strlen(isv("number")) == 11)
