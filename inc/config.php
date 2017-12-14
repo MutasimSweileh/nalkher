46ca8d1af6d3400aef31edcee51a35eef024de69
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-14 20:16:44 +0200

    d

--- inc/config.php
+++ inc/config.php
@@ -1,26 +1,22 @@
 <?php
 $localhost="localhost";
-$UserDb="id3740887_root";    /// ÇÓã ãÓÊÎÏã ÇáÞÇÚÏå
-$PassDb="mohtasm10@@"; ///  ÇáÈÇÓæÑÏ
-$NameDb="id3740887_app";  /// ÇÓã ÇáÞÇÚÏå
+$UserDb="id3740887_root";    /// ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
+$PassDb="mohtasm10@@"; ///  ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
+$NameDb="id3740887_app";  /// ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 /************************************************/
 /*@mysql_connect($localhost,$UserDb,$PassDb)or die('<div style="text-align: center;font-size: 21px;"><p style="font-weight:bold;color:red;">Error</p><br>'.mysql_error()."</div>");
 @mysql_select_db($NameDb);
 @mysql_query("set character_set_server='utf8'");
 mysql_query("SET NAMES 'utf8'");
 date_default_timezone_set("Africa/Cairo");*/
-$url = getenv('JAWSDB_URL2');
-$dbparts = parse_url($url);
-$hostname = $dbparts['host'];
-$username = $dbparts['user'];
-$password = $dbparts['pass'];
-//$database = ltrim($dbparts['path'],'/');
-$database = "pmea2aha671y7tl3";
+//$url = getenv('JAWSDB_URL2');
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
 //echo $database;
-$DBcon = new mysqli($hostname, $username, $password, $database);
+$DBcon = new mysqli($server, $username, $password, $database);
 if ($DBcon->connect_error) {
     die("Connection failed: " . $DBcon->connect_error);
 }
-
-
-?>
