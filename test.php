e1ce350cfb52e1ae963376fc534716fbcba525aa
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-15 00:09:38 +0200

    fff

--- test.php
+++ test.php
@@ -3,16 +3,7 @@ include "inc.php";
 $nofooter = true;
 
 
-$data = array(
-'from' => "mohtasmsawilh1@gmail.com",
-'fromname'  => "Your Name",
-'to' =>$St->email,
-'toname'    => "Example User",
-'subject' =>"test",
-'text' =>"good"
-);
-echo send_mail($data);
-
+echo str_replace('"',"'",get_data($St->url."/assets/js/ajax.js"));
 
 
   ?>
