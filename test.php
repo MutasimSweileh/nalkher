65de396691c4a2c2570884b3f31615653c3cf408
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-15 00:15:05 +0200

    sds

--- test.php
+++ test.php
@@ -1,9 +1,7 @@
 <?php
 include "inc.php";
 $nofooter = true;
-
-
-echo str_replace('"',"'",get_data($St->url."/assets/js/ajax.js"));
-
-
-  ?>
+?>
+<script>
+<?=str_replace('"',"'",get_data($St->url."/assets/js/ajax.js"))?>
+</script>
