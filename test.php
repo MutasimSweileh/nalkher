15bf03de85438e3af498dbb67a6a0080930dbfdf
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-15 02:45:14 +0200

    dfdf

--- test.php
+++ test.php
@@ -8,7 +8,7 @@ $link = $get[$i]["link"];
 $id = $get[$i]["id"];
   UpDate("posts","link",str_replace("http", "https",$link),"where id=".$id);
   if($i == (count($get) - 1)){
-    UpDate("settings","logo",str_replace("http", "https",$St->logo));
+  echo   UpDate("settings","logo",str_replace("http", "https",$St->logo));
   }
 }
 
