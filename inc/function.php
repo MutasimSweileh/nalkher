2a0933971b6437065b76ce4a13c58c5b5629bb4b
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-15 03:41:48 +0200

    a

--- inc/function.php
+++ inc/function.php
@@ -1402,7 +1402,10 @@ function Uimgur($url="", $client_id = '3fd403ffb4414a7')
     $json_returned = curl_exec($curl); // blank response
     $rep =json_decode($json_returned, true);
     if ($rep['success']) {
-        return  array(true,str_replace("http","https",$rep['data']['link'])) ;
+$link = $rep['data']['link'];
+      if(!strpos($rep['data']['link'],"https"));
+      $link = str_replace("http","https",$rep['data']['link']);
+        return  array(true,$link) ;
     } else {
         return  array(false,$rep['data']['error']) ;
     }
--- test.php
+++ test.php
@@ -6,7 +6,7 @@ $get = getUser("posts","where type !='0'");
 for ($i = 0; $i <count($get) ; $i++) {
 $link = $get[$i]["link"];
 $id = $get[$i]["id"];
-  UpDate("posts","link",str_replace("http", "https",$link),"where id=".$id);
+  UpDate("posts","link",str_replace("httpss", "https",$link),"where id=".$id);
   if($i == (count($get) - 1)){
   echo   UpDate("settings","logo",str_replace("http", "https",$St->logo));
   }
