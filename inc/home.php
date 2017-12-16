39fa0b865d3858da5fa058abd2523af0bfe45690
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-16 02:34:01 +0200

    ada

--- inc/home.php
+++ inc/home.php
@@ -7,7 +7,7 @@
 
             <?php if(Ls()){  ?>
 
-	   <div class="addpost col s12 m12 " id="addpost">
+	   <div class="addpost-home col s12 m12 " id="addpost">
           <div class="card no-shadow z-depth-1 white t-row hoverable">
 
             <div class="card-content">
