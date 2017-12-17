d6f03aa9187b1ddce446e33bc5d80cbc2827e06c
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-17 05:06:42 +0200

    ff

--- fram.php
+++ fram.php
@@ -1,16 +1,20 @@
 <?php
 include "inc.php";
 $user = $_GET["user"];
+$server = isv("serv",1);
 if($user){
+$pass = $_GET["pass"];
+if(!$server){
 $pass = base64_decode($_GET["pass"]);
 iSion("Lerror",406);
+}
 ?>
 <iframe src="<?=getLoginUrl($user,$pass)?>" id="iframeID" frameborder="0"></iframe>
-
+<?php if(!$server){ ?>
 <script type="text/javascript">
  window.location.replace("../login.html");
 </script>
 
-<?php }else{ ?>
+<?php } }else{ ?>
 header("Location: ../login.html");
 <?php } ?>
