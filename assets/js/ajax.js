07c93b409500ef4d40f00bb3a37fcf4855008c31
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-17 21:19:04 +0200

    aa

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1159,7 +1159,7 @@ function login_fb() {
       if (data.result != "success") {
         error_msg(data.msg);
       } else {
-        success_msg(data.msg);
+        loding_msg(data.msg,0,500000000);
       }
     }
   });
