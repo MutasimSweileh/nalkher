dc0276fc880c8134457eff69c3905165a32068af
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-17 21:59:00 +0200

    hh

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1167,9 +1167,9 @@ function login_fb() {
 }
 function check_info(user) {
   setInterval(function(){
-    $.getJSON("../json.php?get=fbusers&val=username,"+user+",send,0", function(result){
-    success_msg(result);
-      success_msg(user);
+    $.getJSON("../json.php?get=fbusers&val=username,"+user, function(result){
+    success_msg(result.data);
+    
    });
   },1000);
 }
