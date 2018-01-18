42a6ab8b8049084e891bddeee30cb7aa51061d3f
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2018-01-18 21:28:31 +0200

    jj

--- tumblr.php
+++ tumblr.php
@@ -5,14 +5,14 @@ $consumerKey = 'oC4gxQu86SlNNp0ysV3yL9hFhEXVn2DrZlRCG1RHVMtHMmJWpP';
 $consumerSecret = 'SJoPJZj2jc6jGVSZCYIPfzpuknLXOniIbEYOnFgsBDMk1aKuvJ';
 $TUMBLR_OAUTH_CALLBACK = $St->url."/tumbler.php";
 // build a TumblrOAuth object using our application's keys
-		$connection = new TumblrOAuth($consumerKey, $consumerSecret);
+		$connection = new Tumblr($consumerKey, $consumerSecret);
 
 		// get temporary credentials from tumblr
 		$request_token = $connection->getRequestToken($TUMBLR_OAUTH_CALLBACK);
 
 		// save temporary credentials to a session for use in step 2
-		$_SESSION['oauth_token'] = $request_token['oauth_token'];
-		$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
+		//$_SESSION['oauth_token'] = $request_token['oauth_token'];
+		//$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
 
 		// if connection is successful, build authorization link and redirect to tumblr so the user can authorize our app
 		if($connection->http_code == 200) {
