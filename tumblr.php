5e847b20bece55bc648a413fe808b8b5476758c4
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2018-01-19 11:47:45 +0200

    kkk

--- tumblr.php
+++ tumblr.php
@@ -1,17 +1,11 @@
 <?php
-//include "inc/config.php";
-include_once 'src/facebook.php';
-//require_once('src/oauth/twitteroauth.php');
-require_once 'src/API/src/Google/Client.php';
-require_once 'src/API/src/Google/Service/YouTube.php';
-require_once('Tumblr/tumblroauth/tumblroauth.php');
-//include 'inc/gapi.php';
+include "inc.php";
 $consumerKey = 'oC4gxQu86SlNNp0ysV3yL9hFhEXVn2DrZlRCG1RHVMtHMmJWpP';
 $consumerSecret = 'SJoPJZj2jc6jGVSZCYIPfzpuknLXOniIbEYOnFgsBDMk1aKuvJ';
 $TUMBLR_OAUTH_CALLBACK = $St->url."/tumbler.php";
 
 /* Build TumblrOAuth object with client credentials. */
-$connection = new TumblrOAuth($consumerKey, $consumerSecret);
+$connection = new TwitterOAuth($consumerKey, $consumerSecret);
 
 /* Get temporary credentials. */
 $request_token = $connection->getRequestToken($TUMBLR_OAUTH_CALLBACK);
@@ -25,7 +19,8 @@ switch ($connection->http_code) {
   case 200:
     /* Build authorize URL and redirect user to Tumblr. */
     $url = $connection->getAuthorizeURL($token);
-    header('Location: ' . $url);
+    die($url);
+  //  header('Location: ' . $url);
     break;
   default:
     /* Show notification if something went wrong. */
