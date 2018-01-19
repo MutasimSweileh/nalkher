8605f2c73487150cc72e092e81fd8dca8bea0c70
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2018-01-19 10:58:23 +0200

    jjj

---
+++ Tumblr/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'oC4gxQu86SlNNp0ysV3yL9hFhEXVn2DrZlRCG1RHVMtHMmJWpP');
+define('CONSUMER_SECRET', 'SJoPJZj2jc6jGVSZCYIPfzpuknLXOniIbEYOnFgsBDMk1aKuvJ');
+define('OAUTH_CALLBACK', '../Tumblr/callback.php');
