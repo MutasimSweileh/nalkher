2351d3ff446ce79cbfb1ebfe49368e77895ee869
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2018-01-19 14:06:04 +0200

    hh

--- tumblr.php
+++ tumblr.php
@@ -2,29 +2,28 @@
 include "inc.php";
 $consumerKey = 'oC4gxQu86SlNNp0ysV3yL9hFhEXVn2DrZlRCG1RHVMtHMmJWpP';
 $consumerSecret = 'SJoPJZj2jc6jGVSZCYIPfzpuknLXOniIbEYOnFgsBDMk1aKuvJ';
-$TUMBLR_OAUTH_CALLBACK = $St->url."/tumbler.php";
+$TUMBLR_OAUTH_CALLBACK = $St->url."/tumblr.php";
+if (isset($_REQUEST['oauth_token'])) {
+  $_SESSION['oauth_status'] = 'oldtoken';
+  if ($_SESSION['oauth_token'] !== $_REQUEST['oauth_token'])
+  header('Location: '.$TUMBLR_OAUTH_CALLBACK);
 
-/* Build TumblrOAuth object with client credentials. */
+
+  $connection = new TwitterOAuth($consumerKey, $consumerSecret, $_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
+  $access_token = $connection->getAccessToken($_REQUEST['oauth_verifier']);
+  die($access_token);
+}else{
 $connection = new TwitterOAuth($consumerKey, $consumerSecret,null,null,"tm");
-
-/* Get temporary credentials. */
 $request_token = $connection->getRequestToken($TUMBLR_OAUTH_CALLBACK);
-
-/* Save temporary credentials to session. */
 $_SESSION['oauth_token'] = $token = $request_token['oauth_token'];
 $_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
-
-/* If last connection failed don't display authorization link. */
 switch ($connection->http_code) {
   case 200:
-    /* Build authorize URL and redirect user to Tumblr. */
     $url = $connection->getAuthorizeURL($token);
-    die($url);
-  //  header('Location: ' . $url);
+     header('Location: ' . $url);
     break;
   default:
-    /* Show notification if something went wrong. */
     echo 'Could not connect to Tumblr. Refresh the page or try again later.';
 }
-
+}
 ?>
