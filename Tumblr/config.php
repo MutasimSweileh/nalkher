5c8f1535004be37462b13df385b8d874e773d140
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2018-01-19 11:01:20 +0200

    hh

--- Tumblr/config.php
+++ Tumblr/config.php
@@ -4,7 +4,7 @@
  * @file
  * A single location to store configuration.
  */
-
+include "../inc.php";
 define('CONSUMER_KEY', 'oC4gxQu86SlNNp0ysV3yL9hFhEXVn2DrZlRCG1RHVMtHMmJWpP');
 define('CONSUMER_SECRET', 'SJoPJZj2jc6jGVSZCYIPfzpuknLXOniIbEYOnFgsBDMk1aKuvJ');
-define('OAUTH_CALLBACK', '../Tumblr/callback.php');
+define('OAUTH_CALLBACK', $St->url.'/Tumblr/callback.php');
--- inc.php
+++ inc.php
@@ -6,7 +6,6 @@ $lg='ar';
 include "inc/config.php";
 include "inc/lang.php";
 include "inc/function.php";
-include 'src/Tumblr/API/Client.php';
 //include 'src/lib/tumblrPHP.php';
 include_once 'src/facebook.php';
 require_once('src/oauth/twitteroauth.php');
