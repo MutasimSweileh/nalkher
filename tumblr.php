d2af385e22eeb387d6d912a2eb3b46616afa2b5c
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2018-01-21 13:53:32 +0200

    gg

--- tumblr.php
+++ tumblr.php
@@ -12,7 +12,10 @@ if (isset($_REQUEST['oauth_token'])) {
   $access_token = $connection->getAccessToken($_REQUEST['oauth_verifier']);
   $connection = new TumblrOAuth($consumerKey, $consumerSecret, $access_token['oauth_token'], $access_token['oauth_token_secret']);
   $user_info = $connection->get('user/info');
-  print_r($user_info);
+  SqlIn("Tumblr",array("oauth_username"=>$user_info->response->user->name,"oauth_token"=>$access_token['oauth_token'],"oauth_secret"=>$access_token['oauth_token_secret']));
+  //print_r($user_info);
+ $res =  $connection->post('blog/how2beadad/post', array('type' => 'photo',"source"=>"https://www.baldingbeards.com/wp-content/uploads/2016/09/best-body-hair-trimmer-1-768x512.jpg","link"=>$St->url, 'caption' => 'Text of post here'));
+ print_r($res);
 }else{
 $connection = new TumblrOAuth($consumerKey, $consumerSecret);
 $request_token = $connection->getRequestToken($TUMBLR_OAUTH_CALLBACK);
