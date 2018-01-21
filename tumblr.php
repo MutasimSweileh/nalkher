7609e574f9375dd613dfe2049b457b0335079add
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2018-01-21 13:08:53 +0200

    jj

--- tumblr.php
+++ tumblr.php
@@ -21,7 +21,6 @@ $_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
 switch ($connection->http_code) {
   case 200:
     $url = $connection->getAuthorizeURL($token);
-    //die($url);
     header('Location: ' . $url);
     break;
   default:
